4c18eb979276107b1834c61b3d83c09978920afd
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2024-04-04 19:18:16 -0800

    Time: 196 ms (34.07%), Space: 18.2 MB (50.00%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,12 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # loop through each item in the array
+        for i in range(len(nums)):
+            # check if element at the current position matches the target
+            if nums[i] == target:
+                # if target found, return index
+                return i
+        # otherwise return -1
+        return -1
+            
+        
