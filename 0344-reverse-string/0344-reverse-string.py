9e3716599fbe47cfbe1f2190331cdbc8e83f843d
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2024-04-05 16:56:23 -0800

    Time: 172 ms (44.70%), Space: 20.9 MB (72.65%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        # Using Two Pointer
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+        return s
+    
+        # One line solution
+        #return s.reverse()
+    
+    
+
+        
