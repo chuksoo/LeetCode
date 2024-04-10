5c21fb97e8b0ca8b654b5412c87de2347a4a6fa0
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2024-04-09 19:46:10 -0800

    Time: 939 ms (53.60%), Space: 0B (100.00%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,7 +1,13 @@
 # Write your MySQL query statement below
-WITH customer_count AS (
-    SELECT customer_number, COUNT(customer_number) AS order_count
-    FROM Orders
-    GROUP BY 1
-    )
-SELECT customer_number FROM customer_count ORDER BY order_count DESC LIMIT 1
+# WITH customer_count AS (
+#     SELECT customer_number, COUNT(customer_number) AS order_count
+#     FROM Orders
+#     GROUP BY 1
+#     )
+# SELECT customer_number FROM customer_count ORDER BY order_count DESC LIMIT 1
+
+SELECT customer_number
+FROM Orders
+GROUP BY 1
+HAVING COUNT(*) > 1
+ORDER BY COUNT(*) DESC LIMIT 1
