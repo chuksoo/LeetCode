c6255639df8a4fb5463e492791b8819b5d15d021
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2024-04-10 11:58:37 -0800

    Time: 651 ms (14.77%), Space: 0B (100.00%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 SELECT id, movie, description, rating
 FROM Cinema
-WHERE MOD(id, 2) <> 0 AND description != 'boring' ## MOD() returns remainder of a number divided by another number
-ORDER BY -rating 
+WHERE id % 2 != 0 AND description <> 'boring'
+ORDER BY -rating
