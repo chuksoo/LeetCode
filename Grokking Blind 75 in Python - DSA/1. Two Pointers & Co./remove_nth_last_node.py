171428943d58b216ab245b2dd7a17aee37f84773
chuksoo <mexx4me@yahoo.com>
2024-04-18 18:39:47 -0800

    Two pointers - remove nth node from end of linked list

--- Grokking Blind 75 in Python - DSA/1. Two Pointers & Co./remove_nth_last_node.py
+++ Grokking Blind 75 in Python - DSA/1. Two Pointers & Co./remove_nth_last_node.py
@@ -2,21 +2,26 @@ from linked_list import LinkedList
 from print_list import print_list_with_forward_arrow
 
 def remove_nth_last_node(head, n):
+    # Two pointers, right and left, are set at the head node
     right = head
     left = head
 
+    # Move the right pointer n steps forward
     for i in range(n):
         right = right.next
-    
+
+    # If right reaches NULL, return head's next node
     if not right:
         return head.next
     
+    # Move both right and left pointers forward till right reaches the last node
     while right.next:
         right = right.next
         left = left.next
-
+    
+    # Relink the left node to the node at left's next to the next node
     left.next = left.next.next
-
+    # Return head
     return head
 
 # Driver code
@@ -37,6 +42,11 @@ def main():
         print_list_with_forward_arrow(result)
         print()
         print("-"*100)
+
+"""
+Time complexity: O(n)
+Space complexity: O(1)
+"""
 
 if __name__ == '__main__':
     main()
