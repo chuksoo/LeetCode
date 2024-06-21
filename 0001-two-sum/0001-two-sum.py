e997b51926c856b4f7fcbcc9145f4b0990d7e797
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2024-06-21 08:33:30 -0800

    Time: 67 ms (43.85%), Space: 17.9 MB (8.16%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,16 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_dict = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in num_dict:
-                return [num_dict[complement], i]
-            num_dict[num] = i
+    def twoSum(self, nums: List[int], target: int) -> List[int]: 
+        sorted_nums = sorted((num, i) for i, num in enumerate(nums))
+        
+        low = 0
+        high = len(sorted_nums) - 1
+        while low < high:
+            twins = sorted_nums[low][0] + sorted_nums[high][0]
+            if twins == target:
+                return [sorted_nums[low][1], sorted_nums[high][1]]
+            elif twins < target:
+                low += 1
+            else:
+                high -= 1        
         return []
+      
