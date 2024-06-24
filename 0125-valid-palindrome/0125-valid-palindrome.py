1e75cd30ad7c4e40181a1b0edf701ca9969dfb39
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2024-06-24 14:22:00 -0800

    Time: 47 ms (45.09%), Space: 21.7 MB (10.23%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -3,7 +3,6 @@ class Solution:
         new_lst = [i.lower() for i in s if i.isalnum()]
         left = 0
         right = len(new_lst) - 1 
-        print(new_lst)
         while left < right:
             if new_lst[left] != new_lst[right]:
                 return False
