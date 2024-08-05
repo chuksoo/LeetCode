6615ba3ad045b5cb603ef3a13d7a49181d76bb64
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2024-08-04 17:24:18 -0800

    Time: 194 ms (37.00%) | Memory: 18 MB (64.57%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -4,11 +4,11 @@ class Solution:
         right = len(nums) - 1
         
         while left <= right:
-            mid = (left + right) // 2
+            mid = left + ((right - left) // 2)
             
             if nums[mid] == target:
                 return mid
-            elif nums[mid] < target:
+            elif target > nums[mid]:
                 left = mid + 1
             else:
                 right = mid - 1
