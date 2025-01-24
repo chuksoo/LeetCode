05b27b5cd15494fd5e874e2a6a2edc1eb71c632f
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-01-24 14:02:59 -0900

    Time: 0 ms (100.00%) | Memory: 17.7 MB (36.74%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,16 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # initialize three pointers: start, current, end
         start, current, end = 0, 0, len(nums) - 1
+        # iterate until current crosses the end pointer
         while current <= end:
+            # if current element is 0, swap current with start element 
             if nums[current] == 0:
                 nums[current], nums[start] = nums[start], nums[current]
+                # increment current and start
                 current += 1
                 start += 1
+            # if current element is 1, leave it in place and increment current
             elif nums[current] == 1:
                 current += 1
+            # if current element is 2, swap current with end
             else:
                 nums[current], nums[end] = nums[end], nums[current]
+                # decrement end
                 end -= 1
+        # return nums (which is sorted in place)
         return nums
-        
