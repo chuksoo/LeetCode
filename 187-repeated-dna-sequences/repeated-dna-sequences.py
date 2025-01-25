6b45a9834d94701d697326bc8a950b91639c586d
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-01-24 16:46:09 -0900

    Time: 21 ms (78.66%) | Memory: 29.3 MB (12.05%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/repeated-dna-sequences.py
@@ -1,14 +1,13 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        k = 10
         # create a set to store unique sequence
         unique_sequence = set()
         # create a set to store repeated sequence
         repeated_sequence = set()
         # iterate through the DNA sequence
-        for i in range(0, len(s) - k + 1):
+        for i in range(0, len(s) - 10 + 1):
             # extract substing of length k
-            substring = s[i:i + k]
+            substring = s[i:i + 10]
             # if substring in unique sequence, add to repeated
             if substring in unique_sequence:
                 repeated_sequence.add(substring)
