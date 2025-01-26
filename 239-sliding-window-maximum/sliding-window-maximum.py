0143b27b85434c36b8cd5077d6d4fe237456ea8c
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-01-25 15:32:35 -0900

    Time: 163 ms (69.66%) | Memory: 31.9 MB (36.83%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,23 +1,32 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        output = []
-        queue = collections.deque() # index
-        left = right = 0
-
-        while right < len(nums):
-            # pop smaller values from queue
-            while queue and nums[queue[-1]] < nums[right]:
-                queue.pop()
-            queue.append(right)
-
-            # remove left val from window
-            if left > queue[0]:
+        output = [] # list to store the maximum values for each window
+        queue = collections.deque() # deque to store indices of elements within the current window
+        
+        # Iterate over each element in the input array by its index
+        for i in range(len(nums)):
+            # Remove indices of elements that are out of the current window
+            # If the index at the front of the deque is less than or equal to (i - k), remove it
+            while queue and queue[0] <= i - k:
                 queue.popleft()
-
-            if (right + 1) >= k:
+            
+            # Remove indices of elements from the back of the deque
+            # These elements are smaller than or equal to the current element nums[i],
+            # as they cannot be the maximum of the current or any future window
+            while queue and nums[queue[-1]] <= nums[i]:
+                queue.pop()
+            
+            # Add the current index to the deque
+            # It is a candidate for being the maximum of the current or future windows
+            queue.append(i)
+            
+            # Once the first window of size k is fully processed (i >= k - 1),
+            # the maximum value of the window is at the front of the deque
+            if i >= k - 1:
                 output.append(nums[queue[0]])
-                left += 1
-            right += 1
+
+        # Return the list of maximum values for each sliding window
         return output
+
 
         
