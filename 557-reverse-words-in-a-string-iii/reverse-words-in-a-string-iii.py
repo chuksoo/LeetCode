8247226196b69107aea3f65bdea760f40c69fada
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-02-06 16:26:35 -0900

    Time: 0 ms (100.00%) | Memory: 18.6 MB (10.59%) - LeetSync

---
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        sentence = s.split(' ')
+        reversed_word = [item[::-1] for item in sentence]
+        return " ".join(reversed_word)
+    
+
+        
