f4c18be44ca424e5169346a5692cbb18e8411e2b
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-02-06 16:44:53 -0900

    Time: 2 ms (61.92%) | Memory: 18.4 MB (43.72%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
@@ -1,10 +1,29 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
+        # Brute Force - Step 1
         # sentence = s.split(' ')
         # reversed_word = [item[::-1] for item in sentence]
         # return " ".join(reversed_word)
         
+        # Method 2 - List comprehension
         return " ".join([item[::-1] for item in s.split(' ')])
+
+        # Method 3 - Two Pointer
+#         new_str = ''
+#         for i, item in enumerate(s.split(' ')):
+
+#             new_str += reverse_string(item.split())
+#         return new_str
+
+
+
+# def reverse_string(strg):
+#     start = 0
+#     end = len(strg) - 1
+#     while start <= end:
+#         strg[start], strg[end] = strg[end], strg[start]
+#         start += 1
+#         end -= 1
 
     
 
