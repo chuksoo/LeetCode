0f25863b4038519738bf21c8089774cf2c7b2018
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-02-08 09:29:48 -0900

    Time: 7 ms (26.34%) | Memory: 18.4 MB (88.79%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,5 +1,6 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # if numbers is empty, return empty list
         if not numbers:
             return numbers
 
@@ -10,13 +11,13 @@ class Solution:
         result = []
         # iterate through and check if number at both end add up to target
         while start <= end:
-            # if they do append [start, end] indexes
+            # if they do, append [start index + 1, end index + 1] to the list and return result
             two_sums = numbers[start] + numbers[end]
             if two_sums == target:
                 result.append(start + 1)
                 result.append(end + 1)
                 return result
-            # if their sum is smaller than target, increase left pointer by 1
+            # else if their sum is smaller than target, increase left pointer by 1
             elif two_sums < target:
                 start += 1
             # else decrement right pointer by 1
