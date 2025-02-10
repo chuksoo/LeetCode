74fe5e5cee1069bb9a2dd26e72b60e1ca435ac09
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-02-10 00:52:37 -0900

    Time: 11 ms (58.22%) | Memory: 36.8 MB (64.90%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.py
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -2,13 +2,13 @@ class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
         # initialize pointers
         i, j = 0, 0
-        while i <= len(nums1) - 1 and j <= len(nums2) - 1:
+        while i < len(nums1) and j < len(nums2):
             if nums1[i] < nums2[j]:
                 i += 1
             elif nums1[i] > nums2[j]:
                 j += 1
-            elif nums1[i] == nums2[j]:
+            else: # nums1[i] == nums2[j]:
                 return nums1[i]
-        return -1
+        return -1 # No common value found
 
         
