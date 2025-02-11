c93f5e3b543a98cc996719e046aef03121ab8615
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-02-11 13:12:49 -0900

    Time: 0 ms (100.00%) | Memory: 17.7 MB (48.62%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.py
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.py
@@ -1,7 +1,17 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
-        prefix_sum = [0]
+        # One Pass - Time: O(N), Space: O(N)
+        # prefix_sum = [0]
+        # for num in gain:
+        #     prefix_sum.append(num + prefix_sum[-1])
+        # return max(prefix_sum)
+
+        # Simple, faster approach - Time: O(N), Space: O(1)
+        max_altitude = 0
+        current_altitude = 0
         for num in gain:
-            prefix_sum.append(num + prefix_sum[-1])
-        return max(prefix_sum)
+            current_altitude += num
+            if current_altitude > max_altitude:
+                max_altitude = current_altitude
+        return max_altitude
         
