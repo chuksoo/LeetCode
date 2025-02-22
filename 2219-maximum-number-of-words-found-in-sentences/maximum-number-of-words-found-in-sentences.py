364fe0e5825f0562e3d52376b5c501f0b57367a3
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-02-22 10:55:53 -0900

    Time: 0 ms (100.00%) | Memory: 18 MB (33.25%) - LeetSync

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.py
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.py
@@ -1,13 +1,13 @@
 class Solution:
     def mostWordsFound(self, sentences: List[str]) -> int:
-        word_len, max_word = 0, -inf
-        for word in sentences:
-            word_len = len(word.split())
-            if word_len > max_word:
-                max_word = word_len
-        return max_word
-        # max_len = 0
+        # word_len, max_word = 0, -inf
         # for word in sentences:
-        #     max_len = max(max_len, len(word.split()))
-        # return max_len
+        #     word_len = len(word.split())
+        #     if word_len > max_word:
+        #         max_word = word_len
+        # return max_word
+        max_len = 0
+        for word in sentences:
+            max_len = max(max_len, len(word.split()))
+        return max_len
         
