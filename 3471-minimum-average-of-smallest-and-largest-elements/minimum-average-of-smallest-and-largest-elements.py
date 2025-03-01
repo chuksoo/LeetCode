24e1155816dd87920944e10fff2340397090b263
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-03-01 10:29:28 -0900

    Time: 3 ms (48.86%) | Memory: 17.7 MB (91.81%) - LeetSync

--- 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.py
+++ 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.py
@@ -1,15 +1,27 @@
 class Solution:
     def minimumAverage(self, nums: List[int]) -> float:
-        from collections import deque
-        avgs = []
+        # Time: O(N log N), Space: O(N)
+        # from collections import deque
+        # avgs = []
+        # nums.sort()
+        # nums = deque(nums)
+        # count, n = 0, len(nums)
+        # while nums and count != n:
+        #     min_elem = nums.popleft()
+        #     max_elem = nums.pop()
+        #     avgs.append((min_elem + max_elem) / 2)
+        #     count += 1
+        # return min(avgs)
+
+        # Time: O(N log N), Space: O(N)
         nums.sort()
-        nums = deque(nums)
-        count, n = 0, len(nums)
-        while nums and count != n:
-            min_elem = nums.popleft()
-            max_elem = nums.pop()
-            avgs.append((min_elem + max_elem) / 2)
-            count += 1
-        return min(avgs)
+        left, right = 0, len(nums) - 1
+        min_avg = float('inf')
+        while left < right:
+            min_avg = min(min_avg, ((nums[left] + nums[right])/ 2))
+            left += 1
+            right -= 1
+        return min_avg
+
 
         
