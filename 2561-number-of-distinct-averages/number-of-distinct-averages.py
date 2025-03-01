ed8c7b8e249d03b4c4b0ccf8953cc9153e558060
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-03-01 10:12:19 -0900

    Time: 0 ms (100.00%) | Memory: 17.7 MB (91.54%) - LeetSync

--- 2561-number-of-distinct-averages/number-of-distinct-averages.py
+++ 2561-number-of-distinct-averages/number-of-distinct-averages.py
@@ -1,10 +1,22 @@
 class Solution:
     def distinctAverages(self, nums: List[int]) -> int:
+        # Not efficient - Time: O(N log N), Space: O(N)
+        # nums.sort()
+        # distinct_avg = set()
+        # while nums:
+        #     min_val = nums.pop(0)
+        #     max_val = nums.pop(-1)
+        #     avgs = (min_val + max_val) / 2
+        #     distinct_avg.add(avgs)
+
+        # Time: , Space:
+        from collections import deque
         nums.sort()
+        nums = deque(nums)
         distinct_avg = set()
         while nums:
-            min_val = nums.pop(0)
-            max_val = nums.pop(-1)
+            min_val = nums.popleft()
+            max_val = nums.pop()
             avgs = (min_val + max_val) / 2
             distinct_avg.add(avgs)
         return len(distinct_avg)
