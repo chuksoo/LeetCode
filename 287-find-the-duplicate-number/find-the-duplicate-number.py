d67fb42af15a2d53b3d684f0e8a148bd5d449a1e
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-03-03 11:58:52 -0900

    Time: 40 ms (26.66%) | Memory: 36.7 MB (10.53%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,16 +1,34 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        from collections import Counter
-        duplicated_map = Counter(nums)
-        for key in duplicated_map.keys():
-            if duplicated_map[key] > 1:
-                return key
+        # Brute Force - Time: O(N), Space: O(N)
+        # from collections import Counter
+        # duplicated_map = Counter(nums)
+        # for key in duplicated_map.keys():
+        #     if duplicated_map[key] > 1:
+        #         return key
 
-        # for item in nums:
-        #     if item in duplicated_map:
-        #         duplicated_map[item] += 1
-        #         return item
-        #     else:
-        #         duplicated_map[item] = 1
+        # Two Pointer - Time: O(N), Space: 
+        seen = set()
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            if nums[left] == nums[right]:
+                return nums[left]
+            
+            if nums[left] not in seen and nums[right] not in seen:
+                seen.add(nums[left])
+                seen.add(nums[right])
+                left += 1
+                right -= 1
+            elif nums[left] in seen and nums[right] not in seen:
+                seen.add(nums[right])
+                right -= 1
+                return nums[left]
+            else:
+                seen.add(nums[left])
+                left += 1
+                return nums[right]
+            
         
         
