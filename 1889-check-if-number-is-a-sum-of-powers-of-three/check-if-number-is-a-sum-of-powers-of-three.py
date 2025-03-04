548fa29a4e91d046cfbfe81a422b00bb072f08fa
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-03-04 09:31:28 -0900

    Time: 0 ms (100.00%) | Memory: 17.8 MB (59.96%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.py
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.py
@@ -1,10 +1,9 @@
 class Solution:
     def checkPowersOfThree(self, n: int) -> bool:
-        ternary_base = []
         while n > 0:
             n, r = divmod(n, 3)
-            ternary_base.append(r)
-        return True if 2 not in ternary_base else False
-
+            if r == 2:
+                return False
+        return True 
 
         
