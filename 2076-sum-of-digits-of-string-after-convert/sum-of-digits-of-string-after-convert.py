ba92c68eed8882573c4c893494a11a079be3f56b
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-03-03 15:01:41 -0900

    Time: 3 ms (65.38%) | Memory: 17.8 MB (57.16%) - LeetSync

---
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.py
@@ -0,0 +1,19 @@
+class Solution:
+    def getLucky(self, s: str, k: int) -> int:
+        digit = 0
+        digit = ''.join(str(ord(char) - 96) for char in s)
+        digit = int(digit)
+
+        # Transformation
+        while k > 0:
+            digit = self.get_total_sum(digit)
+            k -= 1
+        return digit
+
+    def get_total_sum(self, val):
+        total_sum = 0
+        while val != 0:
+            quotient, remainder = divmod(val, 10)
+            total_sum += remainder
+            val = quotient
+        return total_sum
