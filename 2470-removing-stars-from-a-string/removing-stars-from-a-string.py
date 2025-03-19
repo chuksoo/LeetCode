4b83ba647faa3e78d301c2b3c0d4b7dff259b06a
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-03-19 14:39:14 -0800

    Time: 148 ms (26.76%) | Memory: 19 MB (59.81%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -7,7 +7,17 @@ class Solution:
             else:
                 if len(star_stack) > 0:
                     star_stack.pop()
-        return ''.join(star_stack)
+
+        result = ""
+        start, end = 0, len(star_stack) - 1
+        while start < end:
+            star_stack[start], star_stack[end] = star_stack[end], star_stack[start]
+            start += 1
+            end -= 1
+
+        while star_stack:
+            result += star_stack.pop()
+        return result
 
 
 
