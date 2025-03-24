d3b995c56b21db82ee120393d7a120622bff0bb5
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-03-24 15:16:23 -0800

    Time: 111 ms (82.24%) | Memory: 24.8 MB (52.70%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,4 +1,14 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        return sorted(nums)
+        import heapq
+
+        # heapify data
+        heapq.heapify(nums)
+
+        heap_lst = []
+        while nums:
+            smallest = heapq.heappop(nums)
+            heap_lst.append(smallest)
+        return heap_lst
+
         
