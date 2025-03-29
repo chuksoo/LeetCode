2050b16e27ef41686ce39c0bf47782532f752f59
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-03-28 17:08:08 -0800

    Time: 19 ms (49.31%) | Memory: 18.7 MB (51.09%) - LeetSync

---
+++ 495-teemo-attacking/teemo-attacking.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
+        if len(timeSeries) == 0:
+            return 0
+
+        total = 0
+        for i in range(0, len(timeSeries) - 1):
+            total += min(timeSeries[i+1] - timeSeries[i], duration)
+        return total + duration
+
+        
