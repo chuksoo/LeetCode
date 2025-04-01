a5f281732e1475667b44a9ffae5c149f78c162e9
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-03-31 18:01:42 -0800

    Time: 2 ms (15.73%) | Memory: 17.9 MB (38.61%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -5,54 +5,28 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # if list1 is None and list2 is not None:
-        #     return list2
-        # elif list1 is not None and list2 is None:
-        #     return list1
-
-        # dummy_node = ListNode(0)
-        # current = dummy_node
-
-        # while list1 is not None and list2 is not None:
-        #     if list1.val <= list2.val:
-        #         current.next = list1
-        #         list1 = list1.next                
-        #     else:
-        #         current.next = list2
-        #         list2 = list2.next 
-        #     current = current.next
-        
-        # # attach any remaining node        
-        # if list1 is not None:
-        #     current.next = list1
-        # else:
-        #     current.next = list2            
-        # return dummy_node.next
-
-    #def merge_timelines(list1, list2):
         if list1 is None and list2 is not None:
             return list2
         elif list1 is not None and list2 is None:
             return list1
-        
-        merged_node = ListNode(0)
-        merged = merged_node
 
-        node_one = list1
-        node_two = list2
-        while node_one and node_two:
-            if node_one.val <= node_two.val:
-                merged.next = node_one
-                node_one = node_one.next
+        dummy_node = ListNode(0)
+        current = dummy_node
+
+        while list1 is not None and list2 is not None:
+            if list1.val <= list2.val:
+                current.next = list1
+                list1 = list1.next                
             else:
-                merged.next = node_two
-                node_two = node_two.next
-            merged = merged.next
+                current.next = list2
+                list2 = list2.next 
+            current = current.next
         
-        if node_one is not None:
-            merged.next = node_one
+        # attach any remaining node        
+        if list1 is not None:
+            current.next = list1
         else:
-            merged.next = node_two
-        return merged_node.next
+            current.next = list2            
+        return dummy_node.next
 
         
