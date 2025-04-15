749a8b450aaf61bba93e5d25ffd757bf18ebd552
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-04-14 19:12:39 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (71.12%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
@@ -5,26 +5,43 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        lst_stack = []
+        # Time - O(N), Space - O(N)
+        # lst_stack = []
+        # current = head
+        # res = None
+        # popped = None
+
+        # while current is not None:
+        #     if lst_stack and lst_stack[-1] == current.val:
+        #         popped = lst_stack.pop()
+        #     else:
+        #         lst_stack.append(current.val)
+        #         if lst_stack[-1] == popped:
+        #             lst_stack.pop()
+        #     current = current.next
+        
+        # while lst_stack:
+        #     new_node = ListNode(lst_stack.pop())
+        #     new_node.next = res
+        #     res = new_node
+        # return res
+            
+        # Time - O(N), Space - O(1)
+        dummy = ListNode(0)
+        dummy.next = head
+        
+        prev = dummy
         current = head
-        res = None
-        popped = None
 
         while current is not None:
-            if lst_stack and lst_stack[-1] == current.val:
-                popped = lst_stack.pop()
+            if current.next and current.val == current.next.val:
+                while current.next and current.val == current.next.val:
+                    current = current.next
+                prev.next = current.next
             else:
-                lst_stack.append(current.val)
-                if lst_stack[-1] == popped:
-                    lst_stack.pop()
+                prev = prev.next
             current = current.next
-        
-        while lst_stack:
-            new_node = ListNode(lst_stack.pop())
-            new_node.next = res
-            res = new_node
-        return res
-            
+        return dummy.next
 
 
 
