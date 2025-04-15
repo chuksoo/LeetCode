4d2f2549fbfb2500903947dff727b59385ffd2ac
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-04-14 18:49:08 -0800

    Time: 41 ms (33.92%) | Memory: 34.4 MB (95.13%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -5,18 +5,27 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        stack = []
-        current = head
-        while current:
-            stack.append(current.val)
-            current = current.next
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
 
-        left, right = 0, len(stack) - 1
-        while left <= right:
-            if stack[left] != stack[right]:
+        reversed_half = self.reverse_linked_list(slow)
+        left, right = head, reversed_half
+        while right:
+            if left.val != right.val:
                 return False
-            else:
-                left += 1
-                right -= 1
+            left = left.next
+            right = right.next
         return True
+
+    def reverse_linked_list(self, head):
+        current = head
+        prev = None
+        while current is not None:
+            temp_node = current.next # store current's next node
+            current.next = prev      # swap nodes
+            prev = current           # update previous
+            current = temp_node      # update current
+        return prev
         
