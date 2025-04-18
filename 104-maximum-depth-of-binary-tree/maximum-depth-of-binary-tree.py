2f9d6552f2adf9b60cf771e3afbf950aa23863d2
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-04-17 23:37:21 -0800

    Time: 8 ms (5.58%) | Memory: 19 MB (79.47%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -6,30 +6,10 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        """  
-        Uderstand:
-            - what should be the output when there's no node
-            - what should we return if no left and right node
-        Plan:
-        -   Using BFS
-            * if node is None, return 0
-            * if left and right node is None, return 1
-            * if left node is None, return 1 plus recursive call to right node
-            * if right node is None, return 1 plus recursive call to left node
-            * return 1 plus maximum of recursive call to left and right node
-        Implement:
-        """
         if root is None:
             return 0
-        
-        if root.left is None and root.right is None:
-            return 1
-        
-        if root.left is None:
-            return 1 + self.maxDepth(root.right)
-        
-        elif root.right is None:
-            return 1 + self.maxDepth(root.left)
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
-        
+
+        left_height = self.maxDepth(root.left)
+        right_height = self.maxDepth(root.right)  
+        return 1 + max(left_height, right_height)
         
