0849506bd2cede1fca723fc6a900267c41ad5e32
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-08-05 14:52:36 -0800

    Time: 773 ms (84.17%) | Memory: 0B (100.00%) - LeetSync

--- 2335-finding-the-topic-of-each-post/finding-the-topic-of-each-post.sql
+++ 2335-finding-the-topic-of-each-post/finding-the-topic-of-each-post.sql
@@ -2,32 +2,18 @@
 WITH post_cte AS (
     SELECT post_id, CONCAT(' ', content, ' ') AS content
     FROM Posts
-),    
+),
 post_topic_matches AS (
-    SELECT 
-        p.post_id,
-        GROUP_CONCAT(DISTINCT k.topic_id separator ',') AS topic_list
+    SELECT p.post_id, GROUP_CONCAT(DISTINCT k.topic_id separator ',') AS topic_list
     FROM post_cte p
-    JOIN Keywords k 
+    JOIN Keywords k
     WHERE INSTR(p.content, CONCAT(' ', LOWER(k.word), ' ')) > 0
     GROUP BY 1
 )
 
 SELECT 
     DISTINCT a.post_id,
-    IFNULL(b.topic_list, 'Ambiguous!') AS topic
+    IFNULL(b.topic_list, "Ambiguous!") AS topic
 FROM Posts a
 LEFT JOIN post_topic_matches b
 ON a.post_id = b.post_id
-
-/*
--- Step 3: Handle all posts (including those without matches)
-SELECT 
-    p.post_id,
-    COALESCE(a.topic_list, 'Ambiguous!') AS topic
-FROM Posts p
-LEFT JOIN aggregated_topics a ON p.post_id = a.post_id
-ORDER BY p.post_id;
-*/
-
--- SELECT * FROM post_topic_matches
