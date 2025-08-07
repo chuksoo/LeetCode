18ec4a787313d436433291d1f17070bce8fa4c31
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-08-06 17:16:22 -0800

    Time: 508 ms (29.91%) | Memory: 0B (100.00%) - LeetSync

---
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+WITH all_orders AS (
+    SELECT customer_number, COUNT(customer_number) as customer_orders
+    FROM Orders
+    GROUP BY 1
+    ORDER BY 2 DESC
+)
+
+SELECT customer_number FROM all_orders LIMIT 1
