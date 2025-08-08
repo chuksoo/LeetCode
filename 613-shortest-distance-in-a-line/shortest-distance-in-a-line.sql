7727b1926da5b0587d56efc40846d7984bdc5d3a
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-08-07 17:45:27 -0800

    Time: 253 ms (59.19%) | Memory: 0B (100.00%) - LeetSync

---
+++ 613-shortest-distance-in-a-line/shortest-distance-in-a-line.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+WITH dist_stats AS (
+    SELECT x, 
+        CASE
+            WHEN (LAG(x, 1) OVER (ORDER BY x)) IS NULL THEN 0
+            ELSE LAG(x, 1) OVER (ORDER BY x) 
+        END AS prev_x,
+        LEAD(x, 1) OVER (ORDER BY x) AS next_x
+    FROM Point
+),
+shortest_dist AS (
+    SELECT ABS(x - next_x) AS shortest 
+    FROM dist_stats
+    WHERE next_x IS NOT NULL
+)
+
+SELECT MIN(shortest) AS shortest
+FROM shortest_dist
+
