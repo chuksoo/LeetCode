861846e4d8de258cfd0bf072a6d1a824533a3708
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-08-11 14:58:27 -0800

    Time: 1049 ms (73.71%) | Memory: 0B (100.00%) - LeetSync

--- 2185-accepted-candidates-from-the-interviews/accepted-candidates-from-the-interviews.sql
+++ 2185-accepted-candidates-from-the-interviews/accepted-candidates-from-the-interviews.sql
@@ -1,5 +1,18 @@
 # Write your MySQL query statement below
-WITH candidate_data AS (
+-- WITH candidate_data AS (
+--     SELECT 
+--         DISTINCT c.candidate_id, 
+--         c.years_of_exp, 
+--         SUM(r.score) OVER(
+--             PARTITION BY c.interview_id
+--         ) AS agg_score
+--     FROM Rounds r
+--     JOIN Candidates c
+--     ON r.interview_id = c.interview_id
+-- )
+
+SELECT candidate_id
+FROM (
     SELECT 
         DISTINCT c.candidate_id, 
         c.years_of_exp, 
@@ -9,8 +22,5 @@ WITH candidate_data AS (
     FROM Rounds r
     JOIN Candidates c
     ON r.interview_id = c.interview_id
-)
-
-SELECT candidate_id
-FROM candidate_data
+) AS candidate_data
 WHERE years_of_exp >= 2 AND agg_score > 15
