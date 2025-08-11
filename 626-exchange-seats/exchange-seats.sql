04b23c68c3bf66d4e337db1ea2a66c719bf28fd2
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-08-11 14:33:27 -0800

    Time: 361 ms (35.31%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+WITH prev_next_student AS (
+    SELECT 
+        id, student, 
+        LAG(student, 1) OVER (ORDER BY id) AS prev_student,
+        LEAD(student, 1) OVER (ORDER BY id) AS next_student
+    FROM Seat
+)
+
+SELECT 
+    id, 
+    CASE
+        WHEN (id % 2 != 0 AND next_student IS NOT NULL) THEN next_student
+        WHEN (id % 2 = 0 AND next_student IS NOT NULL) THEN prev_student
+        WHEN (id % 2 != 0 AND next_student IS NULL) THEN student
+        ELSE prev_student
+    END AS student
+FROM prev_next_student
+
+-- SELECT * FROM prev_next_student
