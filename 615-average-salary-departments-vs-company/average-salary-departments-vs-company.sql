c43e25e2fcdc4cd924fc321aff7474476ce9e5ab
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-08-11 13:32:41 -0800

    Time: 484 ms (10.10%) | Memory: 0B (100.00%) - LeetSync

---
+++ 615-average-salary-departments-vs-company/average-salary-departments-vs-company.sql
@@ -0,0 +1,42 @@
+# Write your MySQL query statement below
+WITH all_data AS (
+    SELECT s.id, e.employee_id, e.department_id, s.amount, s.pay_date
+    FROM Employee e
+    JOIN Salary s
+    ON e.employee_id = s.employee_id
+    ORDER BY 5
+),
+company_average_sal AS (
+    SELECT 
+        DATE_FORMAT(pay_date, '%Y-%m') AS pay_month,
+        AVG(amount) AS coy_avg_sal
+    FROM all_data
+    GROUP BY 1
+    ORDER BY 1
+), 
+dept_monthly_avg AS (
+    SELECT 
+        DISTINCT department_id, 
+        DATE_FORMAT(pay_date, '%Y-%m') AS pay_month,
+        AVG(amount) OVER (
+            PARTITION BY department_id, DATE_FORMAT(pay_date, '%Y-%m')
+        ) AS avg_monthly_sal
+    FROM all_data
+    ORDER BY DATE_FORMAT(pay_date, '%Y-%m') ASC
+)
+
+SELECT 
+    d.pay_month, 
+    d.department_id,  
+    CASE    
+        WHEN d.avg_monthly_sal > c.coy_avg_sal THEN 'higher'
+        WHEN d.avg_monthly_sal < c.coy_avg_sal THEN 'lower'
+        ELSE 'same'
+    END AS comparison
+FROM dept_monthly_avg d
+JOIN company_average_sal c
+ON d.pay_month = c.pay_month
+
+
+
+
