acb6a8bdbb582e2d05481fbfc3af949c8f6ab094
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-08-11 16:12:53 -0800

    Time: 801 ms (52.03%) | Memory: 0B (100.00%) - LeetSync

--- 2451-product-sales-analysis-iv/product-sales-analysis-iv.sql
+++ 2451-product-sales-analysis-iv/product-sales-analysis-iv.sql
@@ -3,29 +3,15 @@ WITH product_sales AS (
     SELECT 
         s.user_id, 
         s.product_id, 
-        (s.quantity * p.price) As spending
+        RANK () OVER(PARTITION BY user_id ORDER BY SUM(s.quantity * p.price) DESC) As rnk
     FROM Sales s
     JOIN Product p
     ON s.product_id = p.product_id
-),
-product_analysis AS (
-    SELECT 
-        user_id,
-        product_id,
-        SUM(spending) as amount_spent
-    FROM product_sales
     GROUP BY 1, 2
-    ORDER BY 1 ASC, 3 DESC
-), 
-product_ranking AS (
-    SELECT 
-        *,
-        RANK () OVER(PARTITION BY user_id ORDER BY amount_spent DESC) as rnk
-    FROM product_analysis
 )
 
 SELECT 
     user_id, product_id
-FROM product_ranking
+FROM product_sales
 WHERE rnk = 1
 
