8022cf897513cbe976c0207a15a0fab98f77ed5a
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-08-12 11:47:40 -0800

    Time: 429 ms (5.46%) | Memory: 0B (100.00%) - LeetSync

--- 618-students-report-by-geography/students-report-by-geography.sql
+++ 618-students-report-by-geography/students-report-by-geography.sql
@@ -1,32 +1,33 @@
 # Write your MySQL query statement below
+WITH america_cte AS (
+    SELECT 
+        name AS America,
+        ROW_NUMBER() OVER(ORDER BY name ASC) as row_num
+    FROM Student
+    WHERE continent = 'America' 
+),
+asia_cte AS (
+    SELECT 
+        name AS Asia,
+        ROW_NUMBER() OVER(ORDER BY name ASC) as row_num
+    FROM Student
+    WHERE continent = 'Asia' 
+),
+europe_cte AS (
+    SELECT 
+        name AS Europe,
+        ROW_NUMBER() OVER(ORDER BY name ASC) as row_num
+    FROM Student
+    WHERE continent = 'Europe' 
+)
+
 SELECT 
     am.America,
     aa.Asia,
     eu.Europe 
-FROM 
-    (
-        SELECT 
-            name AS America,
-            ROW_NUMBER() OVER(ORDER BY name ASC) as row_num
-        FROM Student
-        WHERE continent = 'America' 
-    ) AS am
-LEFT JOIN 
-    (
-        SELECT 
-            name AS Asia,
-            ROW_NUMBER() OVER(ORDER BY name ASC) as row_num
-        FROM Student
-        WHERE continent = 'Asia' 
-    ) AS aa
-ON am.row_num = aa.row_num
-LEFT JOIN 
-    (
-        SELECT 
-            name AS Europe,
-            ROW_NUMBER() OVER(ORDER BY name ASC) as row_num
-        FROM Student
-        WHERE continent = 'Europe' 
-    ) AS eu
-ON am.row_num = eu.row_num
+FROM america_cte am
+LEFT JOIN asia_cte aa
+    ON am.row_num = aa.row_num
+LEFT JOIN europe_cte eu
+    ON am.row_num = eu.row_num
 
