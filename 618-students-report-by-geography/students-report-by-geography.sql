3d45898336493f40f5d815bd490deb1585a70dae
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-08-12 11:41:46 -0800

    Time: 288 ms (16.36%) | Memory: 0B (100.00%) - LeetSync

--- 618-students-report-by-geography/students-report-by-geography.sql
+++ 618-students-report-by-geography/students-report-by-geography.sql
@@ -1,40 +1,27 @@
 # Write your MySQL query statement below
-WITH all_data AS (
+WITH america_cte AS (
     SELECT 
-        CASE
-            WHEN continent = 'America' THEN name
-            ELSE NULL
-        END AS America,
-        CASE
-            WHEN continent = 'Asia' THEN name
-            ELSE NULL
-        END AS Asia,
-        CASE
-            WHEN continent = 'Europe' THEN name
-            ELSE NULL
-        END AS Europe
+        name AS America,
+        ROW_NUMBER() OVER(ORDER BY name ASC) as row_num
     FROM Student
-),
-america_cte AS (
-    SELECT 
-        ROW_NUMBER() OVER(ORDER BY America ASC) as row_num,
-        America
-    FROM all_data
-    WHERE America IS NOT NULL
+    WHERE continent = 'America' 
+    ORDER BY 1
 ),
 asia_cte AS (
     SELECT 
-        ROW_NUMBER() OVER(ORDER BY Asia ASC) as row_num,
-        Asia
-    FROM all_data
-    WHERE Asia IS NOT NULL
+        name AS Asia,
+        ROW_NUMBER() OVER(ORDER BY name ASC) as row_num
+    FROM Student
+    WHERE continent = 'Asia' 
+    ORDER BY 1
 ),
 europe_cte AS (
     SELECT 
-        ROW_NUMBER() OVER(ORDER BY Europe ASC) as row_num,
-        Europe
-    FROM all_data
-    WHERE Europe IS NOT NULL
+        name AS Europe,
+        ROW_NUMBER() OVER(ORDER BY name ASC) as row_num
+    FROM Student
+    WHERE continent = 'Europe' 
+    ORDER BY 1
 )
 
 SELECT 
@@ -46,3 +33,4 @@ LEFT JOIN asia_cte aa
     ON am.row_num = aa.row_num
 LEFT JOIN europe_cte eu
     ON am.row_num = eu.row_num
+
