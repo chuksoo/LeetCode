78b18b1dd0adcb8fcacb2b39803cf2207af476b4
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-08-12 15:47:53 -0800

    Time: 0 ms (100.00%) | Memory: 19.1 MB (10.15%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,21 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Brute force - Time: O(N), Space: O(N)
-        # Create an empty dictionary to store complements
-        complement_dict = {}
-        # iterate thru array
-        for i in range(len(nums)):
-            # compute complement
-            complement = target - nums[i]
-            # check if complement exist in dictionary
-            if complement in complement_dict:
-                # if it does, return indices
-                return [complement_dict[complement], i]
-            # else, add current element and its index to dictionary
-            complement_dict[nums[i]] = i
-
-        # if no pair is found, return empty list
+        two_sums = {}
+        for i, val in enumerate(nums):
+            diff = target - val
+            if diff in two_sums:
+                return [two_sums[diff], i]
+            else:
+                two_sums[val] = i
+        
         return []
 
         
-         
