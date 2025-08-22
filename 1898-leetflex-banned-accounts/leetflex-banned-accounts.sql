375ab82975b9e3587c24e4cc1ea55ec2f677562a
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-08-21 16:11:37 -0800

    Time: 521 ms (59.55%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1898-leetflex-banned-accounts/leetflex-banned-accounts.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT 
+    DISTINCT l1.account_id
+FROM LogInfo l1
+CROSS JOIN LogInfo l2 
+WHERE l1.account_id = l2.account_id           -- Same account
+    AND l1.ip_address != l2.ip_address        -- Different IP addresses
+    AND l1.login <= l2.logout                 -- Session 1 starts before Session 2 ends
+    AND l2.login <= l1.logout                 -- Session 2 starts before Session 1 ends
