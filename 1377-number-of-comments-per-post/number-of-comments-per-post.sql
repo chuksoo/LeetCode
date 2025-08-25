a97cc5a809b8b6f736b6290921c84bf890ce6b3c
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-08-25 11:09:00 -0800

    Time: 1506 ms (25.00%) | Memory: 0B (100.00%) - LeetSync

--- 1377-number-of-comments-per-post/number-of-comments-per-post.sql
+++ 1377-number-of-comments-per-post/number-of-comments-per-post.sql
@@ -1,12 +1,14 @@
 # Write your MySQL query statement below
-WITH unique_post_id AS (
+-- WITH unique_post_id AS 
+
+SELECT 
+    u.post_id, 
+    COUNT(DISTINCT s.sub_id) AS number_of_comments
+FROM (
     SELECT DISTINCT sub_id AS post_id
     FROM Submissions
     WHERE parent_id IS NULL
-)
-
-SELECT u.post_id, COUNT(DISTINCT s.sub_id) AS number_of_comments
-FROM unique_post_id u
+) AS u
 LEFT JOIN Submissions s
 ON u.post_id = s.parent_id
 GROUP BY u.post_id
