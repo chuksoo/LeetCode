7a03e7a14e84f941b1e960cf5268ea3dfd75e4ab
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-08-25 11:07:04 -0800

    Time: 1744 ms (13.13%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1377-number-of-comments-per-post/number-of-comments-per-post.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+WITH unique_post_id AS (
+    SELECT DISTINCT sub_id AS post_id
+    FROM Submissions
+    WHERE parent_id IS NULL
+)
+
+SELECT u.post_id, COUNT(DISTINCT s.sub_id) AS number_of_comments
+FROM unique_post_id u
+LEFT JOIN Submissions s
+ON u.post_id = s.parent_id
+GROUP BY u.post_id
+ORDER BY u.post_id ASC
