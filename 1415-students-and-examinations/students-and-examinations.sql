1de925843a7af526d3be11972e857b619d7a2fdd
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-08-26 12:52:05 -0800

    Time: 885 ms (69.01%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -0,0 +1,27 @@
+# Write your MySQL query statement below
+WITH student_subject_cte AS (
+    SELECT 
+        a.student_id, 
+        a.student_name,
+        b.subject_name
+    FROM Students a
+    CROSS JOIN Subjects b
+),
+exam_attended AS (
+    SELECT student_id, subject_name, COUNT(subject_name) AS attended_exams
+    FROM Examinations
+    GROUP BY 1, 2
+)
+
+SELECT 
+    c.student_id,
+    c.student_name,
+    c.subject_name,
+    COALESCE(d.attended_exams, 0) AS attended_exams
+FROM student_subject_cte c
+LEFT JOIN exam_attended d
+ON c.student_id = d.student_id
+AND c.subject_name = d.subject_name
+ORDER BY 1 ASC, 3 ASC
+
+
