124e7c7792fea6361d04d301804b549073be58a7
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-08-26 14:59:47 -0800

    Time: 558 ms (45.61%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1399-page-recommendations/page-recommendations.sql
@@ -0,0 +1,21 @@
+# Write your MySQL query statement below
+WITH friends_with_user_one AS (
+    SELECT user2_id AS users
+    FROM Friendship
+    WHERE user1_id = 1
+
+    UNION ALL
+
+    SELECT user1_id AS users
+    FROM Friendship 
+    WHERE user2_id = 1
+)
+
+SELECT 
+    DISTINCT l.page_id AS recommended_page
+FROM Likes l
+JOIN friends_with_user_one f
+ON f.users = l.user_id
+WHERE l.page_id NOT IN (
+    SELECT page_id FROM Likes WHERE user_id = 1
+)
