723bbc1c51d806c510bb60a6c42a164dd4f9f35d
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-09-01 16:12:55 -0800

    Time: 360 ms (69.60%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1504-get-the-second-most-recent-activity/get-the-second-most-recent-activity.sql
@@ -0,0 +1,50 @@
+# Write your MySQL query statement below
+WITH
+    ranked_activity_cte AS (
+        SELECT 
+            username
+            , activity
+            , startDate
+            , endDate
+            , RANK() OVER(PARTITION BY username ORDER BY startDate DESC) AS rnk
+        FROM UserActivity
+    ),
+    user_activity_count AS (
+        SELECT 
+            username
+            , COUNT(username) AS activity_count
+        FROM ranked_activity_cte
+        GROUP BY 1
+    ), 
+    all_result AS (
+        SELECT 
+            rac.username
+            , rac.activity
+            , rac.startDate
+            , rac.endDate
+            , rac.rnk
+            , uac.activity_count
+        FROM ranked_activity_cte rac
+        LEFT JOIN user_activity_count uac 
+        ON rac.username = uac.username
+    )
+
+SELECT 
+    username
+    , activity
+    , startDate
+    , endDate
+FROM all_result
+WHERE rnk = 2 AND activity_count > 1
+UNION ALL
+SELECT 
+    username
+    , activity
+    , startDate
+    , endDate
+FROM all_result
+WHERE rnk = 1 AND activity_count = 1
+
+
+
+
