985027f684e3270eb9f301060c5ebc6960029313
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-09-03 14:30:40 -0800

    Time: 577 ms (77.50%) | Memory: 0B (100.00%) - LeetSync

--- 1608-calculate-salaries/calculate-salaries.sql
+++ 1608-calculate-salaries/calculate-salaries.sql
@@ -5,24 +5,19 @@ WITH
             company_id
             , MAX(salary) OVER(PARTITION BY company_id) AS max_salary
         FROM Salaries
-    ),
-    company_tax_rate AS (
-        SELECT
-            company_id
-            , max_salary
-            , CASE
-                WHEN max_salary < 1000 THEN 0.0
-                WHEN (max_salary >= 1000 AND max_salary <= 10000) THEN 0.24
-                WHEN max_salary > 10000 THEN 0.49
-            END AS salary_rate
-        FROM company_max_salary_cte
     )
 
 SELECT
     s.company_id
     , s.employee_id
     , s.employee_name
-    , ROUND((s.salary - s.salary * ctr.salary_rate), 0) AS salary
+    , ROUND(
+        (CASE
+            WHEN c.max_salary < 1000 THEN (s.salary - s.salary * 0.0)
+            WHEN (c.max_salary >= 1000 AND c.max_salary <= 10000) THEN (s.salary - s.salary * 0.24)
+            ELSE (s.salary - s.salary * 0.49)
+        END), 0) AS salary
 FROM Salaries AS s
-JOIN company_tax_rate AS ctr
-ON s.company_id = ctr.company_id
+JOIN company_max_salary_cte AS c
+ON s.company_id = c.company_id
+
