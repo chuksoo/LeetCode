a6bd8cae1853105f888dfd7962f5218f96bbe4b7
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-09-06 13:13:45 -0800

    Time: 769 ms (100.00%) | Memory: 0B (100.00%) - LeetSync

--- 1654-customer-order-frequency/customer-order-frequency.sql
+++ 1654-customer-order-frequency/customer-order-frequency.sql
@@ -4,45 +4,21 @@ WITH
         SELECT 
             c.customer_id
             , c.name
-            , o.product_id
-            , p.price
             , DATE_FORMAT(o.order_date, '%Y-%m') AS order_date
-            , o.quantity
-            , (p.price * o.quantity) AS amount_spent
         FROM Orders o
         LEFT JOIN Customers c
             ON c.customer_id = o.customer_id
         LEFT JOIN Product p
             ON p.product_id = o.product_id
-    ),
-    monthly_spend_cte AS (
-        SELECT 
-            customer_id
-            , name
-            , order_date
-            , SUM(amount_spent) AS monthly_spent
-        FROM amount_spent_cte
-        WHERE order_date = '2020-06' OR order_date = '2020-07'
-        GROUP BY 1, 3
+        WHERE DATE_FORMAT(o.order_date, '%Y-%m') IN ('2020-06', '2020-07')
+        GROUP BY 1, 2, 3
+        HAVING SUM(p.price * o.quantity) >= 100
     )
+
 SELECT 
     customer_id
     , name
-FROM (
-    SELECT 
-        DISTINCT customer_id
-        , name
-    FROM monthly_spend_cte
-    WHERE monthly_spent >= 100
-    AND order_date = '2020-06'
-    UNION ALL 
-    SELECT 
-        DISTINCT customer_id
-        , name
-    FROM monthly_spend_cte
-    WHERE monthly_spent >= 100
-    AND order_date = '2020-07'
-) AS freq
-GROUP BY 1
-HAVING COUNT(*) = 2
+FROM amount_spent_cte 
+GROUP BY 1, 2
+HAVING COUNT(order_date) > 1
 
