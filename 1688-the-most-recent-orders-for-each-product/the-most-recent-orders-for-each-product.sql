8bf55601f4905d75cd361c8acebacd451959cae8
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-09-08 14:01:00 -0800

    Time: 2326 ms (45.02%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1688-the-most-recent-orders-for-each-product/the-most-recent-orders-for-each-product.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+WITH 
+    ranked_product_orders_cte AS (
+        SELECT
+            p.product_name
+            , o.product_id
+            , o.order_id
+            , o.order_date
+            , RANK () OVER(PARTITION BY o.product_id ORDER BY o.order_date DESC) AS order_rnk
+        FROM Orders o
+        JOIN Products p
+        ON o.product_id = p.product_id
+    )
+
+SELECT 
+    product_name
+    , product_id
+    , order_id
+    , order_date 
+FROM ranked_product_orders_cte
+WHERE order_rnk = 1
+ORDER BY product_name ASC, product_id ASC, order_id ASC
