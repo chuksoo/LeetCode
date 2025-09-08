5da552d223778a3aff672693a7d060e930860b77
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-09-08 15:35:05 -0800

    Time: 316 ms (92.07%) | Memory: 0B (100.00%) - LeetSync

--- 1607-rectangles-area/rectangles-area.sql
+++ 1607-rectangles-area/rectangles-area.sql
@@ -5,5 +5,5 @@ SELECT
     , ABS(p1.x_value - p2.x_value) * ABS(p1.y_value - p2.y_value) AS area
 FROM Points p1
 CROSS JOIN Points p2
-WHERE ABS(p1.x_value - p2.x_value) * ABS(p1.y_value - p2.y_value) != 0 AND p1.id < p2.id
+HAVING area != 0 AND p1 < p2
 ORDER BY area DESC, p1 ASC, p2 ASC
