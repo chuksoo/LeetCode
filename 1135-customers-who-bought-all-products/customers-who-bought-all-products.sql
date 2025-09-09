1bfffa2eeb14379c4e163ac2dd89d2a36faf23ba
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-09-08 20:42:38 -0800

    Time: 531 ms (81.34%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT
+    c.customer_id
+FROM Customer c
+JOIN Product p
+ON c.product_key = p.product_key
+GROUP BY 1
+HAVING COUNT(DISTINCT c.product_key) = (SELECT COUNT(product_key) FROM Product)
