4eef333e60e58ca152d4b582592152b319783ef2
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-09-09 00:25:42 -0800

    Time: 342 ms (15.75%) | Memory: 0B (100.00%) - LeetSync

--- 3650-find-cities-in-each-state-ii/find-cities-in-each-state-ii.sql
+++ 3650-find-cities-in-each-state-ii/find-cities-in-each-state-ii.sql
@@ -1,16 +1,10 @@
 # Write your MySQL query statement below
-WITH
-    grouped_matching_city_cte AS (
-        SELECT 
-            state
-            , GROUP_CONCAT(DISTINCT city ORDER BY city ASC SEPARATOR ', ') AS cities
-            , SUM(IF(LEFT(state, 1) = LEFT(city, 1), 1, 0)) AS matching_letter_count
-        FROM cities
-        GROUP BY 1
-        HAVING COUNT(city) >= 3 AND matching_letter_count > 0
-    )
-
-SELECT *
-FROM grouped_matching_city_cte
+SELECT 
+    state
+    , GROUP_CONCAT(DISTINCT city ORDER BY city ASC SEPARATOR ', ') AS cities
+    , SUM(IF(LEFT(state, 1) = LEFT(city, 1), 1, 0)) AS matching_letter_count
+FROM cities
+GROUP BY 1
+HAVING COUNT(city) >= 3 AND matching_letter_count > 0
 ORDER BY 3 DESC, 1 ASC
 
