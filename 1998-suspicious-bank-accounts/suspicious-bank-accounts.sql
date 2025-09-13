2cbfa7f9206ea97efbe0906be11d22e7d84ab0bf
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-09-13 12:23:12 -0700

    Time: 1133 ms (25.75%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1998-suspicious-bank-accounts/suspicious-bank-accounts.sql
@@ -0,0 +1,23 @@
+# Write your MySQL query statement below
+WITH
+    monthly_income_cte AS (
+        SELECT
+            account_id
+            , DATE_FORMAT(day, '%Y%m') AS day_month
+            , SUM(amount) AS income
+            , max_income
+        FROM Transactions
+        JOIN Accounts
+        USING(account_id)
+        WHERE type = 'Creditor'
+        GROUP BY 1, 2
+        HAVING income > max_income
+        ORDER BY 1 ASC, 2 ASC
+    )
+
+SELECT 
+    DISTINCT account_id
+FROM monthly_income_cte a
+JOIN monthly_income_cte b
+USING(account_id)
+WHERE PERIOD_DIFF(a.day_month, b.day_month) = 1
