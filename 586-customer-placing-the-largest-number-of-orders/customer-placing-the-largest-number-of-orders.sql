b7fdc107c596b9a6f5ba94754d110b6d5957a6a4
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-09-15 11:16:07 -0700

    Time: 447 ms (80.32%) | Memory: 0B (100.00%) - LeetSync

--- 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -1,9 +1,14 @@
 # Write your MySQL query statement below
 WITH all_orders AS (
-    SELECT customer_number, COUNT(customer_number) as customer_orders
+    SELECT 
+        customer_number
+        , COUNT(customer_number) as customer_orders
     FROM Orders
     GROUP BY 1
     ORDER BY 2 DESC
 )
 
-SELECT customer_number FROM all_orders LIMIT 1
+SELECT 
+    customer_number 
+FROM all_orders 
+LIMIT 1
