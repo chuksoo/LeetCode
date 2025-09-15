bf33db3a9305ef7913f16996bcfb999d4b031b87
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-09-15 10:44:19 -0700

    Time: 844 ms (7.73%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2030-group-employees-of-the-same-salary/group-employees-of-the-same-salary.sql
@@ -0,0 +1,24 @@
+# Write your MySQL query statement below
+WITH
+    employee_with_unique_salary AS (
+        SELECT 
+            employee_id
+            , name
+            , salary
+        FROM Employees
+        GROUP BY 3
+        HAVING COUNT(salary) = 1
+    ),
+    employee_without_unique_salary AS (
+        SELECT * FROM Employees
+        EXCEPT
+        SELECT * FROM employee_with_unique_salary
+    )
+
+SELECT 
+    employee_id
+    , name
+    , salary
+    , DENSE_RANK () OVER(ORDER BY salary ASC) AS team_id
+FROM employee_without_unique_salary
+ORDER BY team_id ASC, employee_id ASC
