e1eb8bf0367c52e271b0ab65d317ecdc6f8087dc
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-09-15 10:09:20 -0700

    Time: 2010 ms (7.89%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,6 +1,11 @@
 # Write your MySQL query statement below
 WITH order_red AS (
-    SELECT s.sales_id, s.name AS red_name, o.order_id, o.com_id, c.name
+    SELECT 
+        s.sales_id
+        , s.name AS red_name
+        , o.order_id
+        , o.com_id
+        , c.name
     FROM SalesPerson s
     LEFT JOIN Orders o ON s.sales_id = o.sales_id
     LEFT JOIN Company c ON c.com_id = o.com_id
