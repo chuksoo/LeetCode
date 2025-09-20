891a6b35d107b789212ae93ac447fb11a2fd3285
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-09-19 17:09:20 -0700

    Time: 888 ms (28.73%) | Memory: 0B (100.00%) - LeetSync

--- 1481-students-with-invalid-departments/students-with-invalid-departments.sql
+++ 1481-students-with-invalid-departments/students-with-invalid-departments.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 SELECT 
-    s.id
-    , s.name
-FROM Students s
-WHERE s.department_id NOT IN (SELECT id FROM Departments)
+    id
+    , name
+FROM Students 
+WHERE department_id NOT IN (SELECT id FROM Departments)
