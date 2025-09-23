c2b98ab05d8817339e3458463a58a6dad852eecb
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-09-23 11:55:52 -0700

    Time: 845 ms (88.47%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -5,9 +5,9 @@ FROM (
         e.name AS Employee, 
         e.salary AS Salary, 
         d.name AS Department, 
-        DENSE_RANK () OVER (PARTITION BY d.name ORDER BY e.salary DESC) AS rank_
+        DENSE_RANK () OVER (PARTITION BY d.name ORDER BY e.salary DESC) AS sal_rank
     FROM Employee e
     JOIN Department d
     ON e.departmentId = d.id 
 ) AS ranked_cte
-WHERE rank_ <= 3
+WHERE sal_rank <= 3
