b0cbca85ea4e867cf73af4bb5ad810e4663149bb
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2025-12-10 14:43:20 -0900

    Time: 263 ms (81.69%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3256-find-candidates-for-data-scientist-position/find-candidates-for-data-scientist-position.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT
+    candidate_id
+FROM candidates
+WHERE skill IN ("Python", "Tableau", "PostgreSQL")
+GROUP BY candidate_id
+HAVING COUNT(candidate_id) = 3
+ORDER BY candidate_id ASC
