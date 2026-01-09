9d20c65cc435e7e43e7467fa0b361ff7d99b40e6
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2026-01-09 13:17:08 -0900

    Time: 0 ms (100.00%) | Memory: 19.3 MB (5.56%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -1,14 +1,6 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        # One Pass - Time: O(N), Space: O(N)
-        # prefix = [nums[0]]
-        # for i in range(1, len(nums)):
-        #     prefix.append(nums[i] + prefix[-1])
-        # return prefix
-
-        # One Pass, In-Place - Time: O(N), Space - O(1)
         for i in range(1, len(nums)):
-            nums[i] = nums[i] + nums[i-1]
+            nums[i] = nums[i] + nums[i - 1]
         return nums
-
         
