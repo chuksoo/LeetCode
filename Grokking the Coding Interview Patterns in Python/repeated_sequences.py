9a65276a541e1260f9bf8c9df44f07f35313e0ea
chuksoo <mexx4me@yahoo.com>
2026-02-09 00:11:36 -0900

    Repeated DNA Sequences

---
+++ Grokking the Coding Interview Patterns in Python/repeated_sequences.py
@@ -0,0 +1,6 @@
+"""
+A DNA sequence consists of nucleotides represented by the letters ‘A’, ‘C’, ‘G’, and ‘T’ only. For example, “ACGAATTCCG” is a valid DNA sequence.
+
+Given a string, s, that represents a DNA sequence, return all the 10-letter-long sequences (continuous substrings of exactly 10 characters) 
+that appear more than once in s. You can return the output in any order.
+"""
